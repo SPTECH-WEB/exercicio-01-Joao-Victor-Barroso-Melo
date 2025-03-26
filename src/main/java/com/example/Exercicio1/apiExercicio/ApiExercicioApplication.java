5a5adca401298af011e7db1398b073db219a6c12
João <joao.melo@sptech.school>
2025-03-26 17:19:40 -0300

    Adicionando estrutura do projeto

---
+++ src/main/java/com/example/Exercicio1/apiExercicio/ApiExercicioApplication.java
@@ -0,0 +1,13 @@
+package com.example.Exercicio1.apiExercicio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiExercicioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiExercicioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Exercicio1/apiExercicio/ApiExercicioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Exercicio1.apiExercicio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiExercicioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
